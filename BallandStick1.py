256089fc1b4daec0973c0db356c167f1ff4a9f26
Rachel Wegener <rachweg@scc1.bu.edu>
2021-07-15 16:55:56 -0400

    got to ball and stick 2

---
+++ BallandStick1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 14:57:00 2021
+
+@author: rachweg
+"""
+from neuron import h
+#from neuron.units import ms, mV
+h.load_file('stdrun.hoc')
+import matplotlib.pyplot as plt
+
+
+class BallAndStick:
+    def __init__(self, gid):
+        self._gid = gid
+        self._setup_morphology()
+        self._setup_biophysics()
+    def _setup_morphology(self):
+        self.soma = h.Section(name='soma', cell=self)
+        self.dend = h.Section(name='dend', cell=self)
+        self.all = [self.soma, self.dend]
+        self.dend.connect(self.soma)
+        self.soma.L = self.soma.diam = 12.6157
+        self.dend.L = 200
+        self.dend.diam = 1
+    def _setup_biophysics(self):
+        for sec in self.all:
+            sec.Ra = 100    # Axial resistance in Ohm * cm
+            sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
+        self.soma.insert('hh')                                          
+        for seg in self.soma:
+            seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+            seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+            seg.hh.gl = 0.0003    # Leak conductance in S/cm2
+            seg.hh.el = -54.3     # Reversal potential in mV
+        # Insert passive current in the dendrite                       # <-- NEW
+        self.dend.insert('pas')                                        # <-- NEW
+        for seg in self.dend:                                          # <-- NEW
+            seg.pas.g = 0.001  # Passive conductance in S/cm2          # <-- NEW
+            seg.pas.e = -65    # Leak reversal potential mV            # <-- NEW 
+    def __repr__(self):
+        return 'BallAndStick[{}]'.format(self._gid)
+
+my_cell = BallAndStick(0)
+
+stim = h.IClamp(my_cell.dend(1))
+stim.delay = 5
+stim.dur = 1
+stim.amp = 0.1
+
+soma_v = h.Vector().record(my_cell.soma(0.5)._ref_v)
+t = h.Vector().record(h._ref_t)
+dend_v = h.Vector().record(my_cell.dend(0.5)._ref_v)
+
+h.finitialize(-65)
+h.continuerun(25)
+
+
+f = plt.figure(x_axis_label='t (ms)', y_axis_label='v (mV)')
+amps = [0.075 * i for i in range(1, 5)]
+colors = ['green', 'blue', 'red', 'black']
+for amp, color in zip(amps, colors):
+    stim.amp = amp
+    for my_cell.dend.nseg, width in [(1, 2), (101, 1)]:
+        h.finitialize(-65)
+        h.continuerun(25)
+        f.line(t, list(soma_v),
+               line_width=width,
+               legend_label='amp=%g' % amp if my_cell.dend.nseg == 1 else None,
+               color=color)
+        f.line(t, list(dend_v),
+               line_width=width,
+               line_dash='dashed',
+               color=color)
+plt.show(f)
+
+
+
+
+
+
+
---
+++ BallandStick3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 15:55:46 2021
+
+@author: rachweg
+"""
+
+
